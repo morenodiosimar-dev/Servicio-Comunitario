011cb7b15e9d3b2f469f629b8d9e2f8ff808a862
Diosimar Moreno <morenodiosimar@gmail.com>
2026-07-18 18:08:38 -0400

    ver

--- api/server.js
+++ api/server.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(process.cwd(), './')));
 
 // Rate limiting para login
 const loginAttempts = {};
